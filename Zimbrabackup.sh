b44076359cedf21e762f94e68443ef9c81453606
Pavan Kumar <pavannani@ymail.com>
2021-05-18 14:47:12 +0530

    Create Zimbrabackup.sh

---
+++ Zimbrabackup.sh
@@ -0,0 +1,14 @@
+echo -n "Please Enter Destination Server IP (New Server) : "
+read NewServerIP
+folder=/tmp/zmigrate/
+mkdir $folder
+cd  $folder
+mkdir userpass userdata alias
+zmprov -l gaa | grep -v galsync | grep -v spam. | grep -v ham. | grep -v virus-quarantine. >emails.txt
+zmprov gad > domains.txt
+for i in `cat emails.txt`; do zmprov  -l ga $i userPassword | grep userPassword: | awk '{ print $2}' > userpass/$i.shadow; done
+for i in `cat emails.txt`; do zmprov ga $i  | grep -i Name: > userdata/$i.txt ; done
+zmprov gaaa > admins.txt
+postconf mynetworks > postconf.txt
+
+rsync -avp -e /tmp/zmigrate  root@$NewServerIP:/tmp/
