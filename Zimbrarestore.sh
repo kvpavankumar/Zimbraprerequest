971bd6ff911b18aedeff52a9620b5e5a1d38839e
Pavan Kumar <pavannani@ymail.com>
2021-05-18 14:52:25 +0530

    Create Zimbrarestore.sh

---
+++ Zimbrarestore.sh
@@ -0,0 +1,17 @@
+for i in `cat /tmp/zmigrate/domains.txt `; do  zmprov cd $i zimbraAuthMech zimbra ;echo $i ;done
+
+USERPASS="/tmp/zmigrate/userpass"
+USERDDATA="/tmp/zmigrate/userdata"
+USERS="/tmp/zmigrate/emails.txt"
+
+for i in `cat $USERS`
+do
+givenName=$(grep givenName: $USERDDATA/$i.txt | cut -d ":" -f2)
+displayName=$(grep displayName: $USERDDATA/$i.txt | cut -d ":" -f2)
+shadowpass=$(cat $USERPASS/$i.shadow)
+tmpPass="CHANGEme"
+zmprov ca $i CHANGEme cn "$givenName" displayName "$displayName" givenName "$givenName"
+zmprov ma $i userPassword "$shadowpass"
+done
+for i in `cat /tmp/zmigrate/admins.txt`; do zmprov ma $i zimbraIsAdminAccount TRUE ; done
+
