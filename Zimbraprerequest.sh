df47f0a5db114d69ecdfc5e636ec8321df66b89b
Pavan Kumar <pavannani@ymail.com>
2021-05-18 14:35:45 +0530

    Update Zimbraprerequest.sh

--- Zimbraprerequest.sh
+++ Zimbraprerequest.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo -n "Please insert your Domain name. : "
+echo -n "Please Enter your Domain name. : "
 read DOMAIN
-echo -n "Please insert your IP Address : "
-read IPADDRESS
-
+IPADDRESS=$(ifconfig | grep 'inet' |  awk '{print $2}' | grep -v '127.0.0.1')
 
 echo -e "Install dependencies"
 sleep 3
@@ -33,3 +31,14 @@ cp /etc/resolvconf/resolv.conf.d/head /etc/resolvconf/resolv.conf.d/head.backup
 echo "search $DOMAIN" > /etc/resolvconf/resolv.conf.d/head
 echo "nameserver $IPADDRESS" >> /etc/resolvconf/resolv.conf.d/head
 echo "nameserver 8.8.8.8" >> /etc/resolvconf/resolv.conf.d/head
+
+
+ wget https://files.zimbra.com/downloads/8.8.15_GA/zcs-8.8.15_GA_3869.UBUNTU18_64.20190918004220.tgz
+ 
+ tar xvf zcs-8.8.15_GA_3869.UBUNTU18_64.20190918004220.tgz
+ 
+ cd zcs-8.8.15_GA_3869.UBUNTU18_64.20190918004220
+ 
+ 
+
+
