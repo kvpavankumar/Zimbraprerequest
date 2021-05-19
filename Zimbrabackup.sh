fc0ee564b3db39a335b5961697c593fcadb8f93f
Pavan Kumar <pavannani@ymail.com>
2021-05-19 10:35:02 +0530

    Update Zimbrabackup.sh

--- Zimbrabackup.sh
+++ Zimbrabackup.sh
@@ -25,7 +25,8 @@ echo -ne '#####                     (10%)\r'
 zmprov -l gaa | grep -v galsync | grep -v spam. | grep -v ham. | grep -v virus-quarantine. >emails.txt
 zmprov gad > domains.txt
 echo -ne '#############             (30%)\r'
-for i in `cat emails.txt`; do zmprov  -l ga $i userPassword | grep userPassword: | awk '{ print $2}' > userpass/$i.shadow; done
+j=1
+for i in `cat emails.txt`; do echo -ne '#############             (30%) Completed Account $J/ Total Account $i\r'; zmprov  -l ga $i userPassword | grep userPassword: | awk '{ print $2}' > userpass/$i.shadow;  done
 for i in `cat emails.txt`; do zmprov ga $i  | grep -i Name: > userdata/$i.txt ; done
 zmprov gaaa > admins.txt
 echo -ne '#####################     (90%)\r'
