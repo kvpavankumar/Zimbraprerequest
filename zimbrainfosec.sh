c2329da6b6b6496afd60238429164f913c3732a7
Pavan Kumar <pavannani@ymail.com>
2023-10-30 12:18:34 +0530

    Update zimbrainfosec.sh

--- zimbrainfosec.sh
+++ zimbrainfosec.sh
@@ -35,4 +35,17 @@ su - zimbra -c 'zmprov mcf zimbraMtaSmtpdTlsCiphers medium'
 su - zimbra -c 'zmprov mcf zimbraMtaSmtpdTlsMandatoryCiphers  medium'
 su - zimbra -c 'zmlocalconfig -e zimbra_zmjava_options="-Xmx256m -Dhttps.protocols=TLSv1.2 -Djdk.tls.client.protocols=TLSv1.2 -Djava.net.preferIPv4Stack=true"'
 su - zimbra -c 'zmlocalconfig -e mailboxd_java_options="-server -Dhttps.protocols=TLSv1.2,TLSv1.3 -Djdk.tls.client.protocols=TLSv1.2,TLSv1.3 -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=${networkaddress_cache_ttl} -Dorg.apache.jasper.compiler.disablejsr199=true -XX:+UseG1GC -XX:SoftRefLRUPolicyMSPerMB=1 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=15 -XX:G1MaxNewSizePercent=45 -XX:-OmitStackTraceInFastThrow -verbose:gc -Xlog:gc*=info,safepoint=info:file=/opt/zimbra/log/gc.log:time:filecount=20,filesize=10m -Djava.net.preferIPv4Stack=true -Djava.security.egd=file:/dev/./urandom --add-opens java.base/java.lang=ALL-UNNAMED"'
+IPADDRESS=$(ifconfig | grep 'inet' |  awk '{print $2}' | grep -v '127.0.0.1')
+su - zimbra -c "zmprov ms $(hostname) zimbraMtaMyNetworks '127.0.0.0/8 $IPADDRESS/32'"
 su - zimbra -c 'zmcontrol restart'
+
+sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/50-default.conf > /etc/rsyslog.d/50-default.conf
+sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/rsyslog> /etc/logrotate.d/rsyslog 
+sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/rsyslog.conf>/etc/rsyslog.conf
+sudo systemctl restart syslog.service
+sudo curl https://raw.githubusercontent.com/kvpavankumar/Zimbrapostfixlogs/master/zimbra_pflogsumm.pl >/opt/zimbra/bin/zimbra_pflogsumm.pl
+chmod 777  /opt/zimbra/bin/zimbra_pflogsumm.pl
+su - zimbra -c "(crontab -l ; echo '00 07 * * * /opt/zimbra/serverinfo.sh') | crontab -"
+(crontab -l ; echo "*/5 * * * * /root/bouncedatadb.sh") | crontab -
+(crontab -l ; echo "00 07 * * * 15 01 * * * /root/bulkdatapavan.sh") | crontab -
+
